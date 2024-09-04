462c2afb95bd4591d45e7a1ebd9fb9cf107514b5
hem1t <sunny10fb@gmail.com>
2024-09-04 10:25:04 +0530

    break time

--- src/projects/project04/RapidXO.jsx
+++ src/projects/project04/RapidXO.jsx
@@ -1,10 +1,17 @@
+import { useReducer, useState } from "react";
 import "./RapidXO.css";
 
-const XOBoard = ({ board }) => {
+const XOBoard = ({ board, onCellClick }) => {
   return (
     <div className="xo-board">
       {board.map((val, i) => (
-        <div className="xo-board-cell" key={i}>
+        <div
+          className="xo-board-cell"
+          key={i}
+          onClick={() => {
+            onCellClick(i);
+          }}
+        >
           {val}
         </div>
       ))}
@@ -12,11 +19,41 @@ const XOBoard = ({ board }) => {
   );
 };
 
+// action:
+// {
+//    board: 0 | 1 | 2,
+//    pos: 0 - 9,
+//    val: X | O
+// }
+
+function board_reducer(boards, action) {}
+
 const RapidXO = () => {
+  // 0, haven't started.
+  // 1, user is playing.
+  // 2, computer is playing.
+  // 3, result state.
+
+  let [] = useState(0);
+  let [boards, do_on_boards] = useReducer(board_reducer, [
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+  ]);
+
   return (
     <div className="full-page">
-      <div className="rapid-xo-page">
-        <XOBoard board={["1", "1", "1", "1", "1", "1", "1", "1", "1"]} />
+      <div className="rapid-xo-component">
+        {boards.map((board, i) => (
+          <XOBoard
+            board={board}
+            key={i}
+            onCellClick={(k) => {
+              console.log("Clicked: ", k + " from " + i);
+            }}
+          />
+        ))}
       </div>
     </div>
   );
